13acee1c5c3b22a8e4d1122fd592330eb4baf31e
William Brown <william@blackhats.net.au>
2022-05-20 16:06:02 +1000

    Add experimental concurrent cache support

--- src/cache/cache.rs
+++ src/cache/cache.rs
@@ -14,6 +14,8 @@
 
 #[cfg(feature = "azure")]
 use crate::cache::azure::AzureBlobCache;
+#[cfg(feature = "concurrent-cache")]
+use crate::cache::conccache::ConcurrentDiskCache;
 use crate::cache::disk::DiskCache;
 #[cfg(feature = "gcs")]
 use crate::cache::gcs::{GCSCache, RWMode};
@@ -36,7 +38,8 @@ use crate::config::Config;
     feature = "memcached",
     feature = "redis",
     feature = "s3",
-    feature = "webdav"
+    feature = "webdav",
+    feature = "concurrent-cache"
 ))]
 use crate::config::{self, CacheType};
 use async_trait::async_trait;
@@ -645,6 +648,24 @@ pub fn storage_from_config(
 
                 return Ok(Arc::new(storage));
             }
+            CacheType::ConcurrentDisk(config::ConcurrentDiskCacheConfig {
+                ref dir,
+                ref size,
+                ref durable_fs,
+            }) => {
+                debug!(
+                    "Trying ConcurrentDiskCache({:?}, {}, {})",
+                    dir, size, durable_fs
+                );
+                #[cfg(feature = "concurrent-cache")]
+                match ConcurrentDiskCache::new(dir, *size, *durable_fs, pool) {
+                    Ok(s) => {
+                        trace!("Using ConcurrentDiskCache");
+                        return Ok(Arc::new(s));
+                    }
+                    Err(e) => warn!("Failed to create ConcurrentDiskCache: {:?}", e),
+                }
+            }
             #[allow(unreachable_patterns)]
             _ => bail!("cache type is not enabled"),
         }
@@ -653,12 +674,14 @@ pub fn storage_from_config(
     let (dir, size) = (&config.fallback_cache.dir, config.fallback_cache.size);
     let preprocessor_cache_mode_config = config.fallback_cache.preprocessor_cache_mode;
     debug!("Init disk cache with dir {:?}, size {}", dir, size);
-    Ok(Arc::new(DiskCache::new(
-        dir,
-        size,
-        pool,
-        preprocessor_cache_mode_config,
-    )))
+
+    #[cfg(feature = "concurrent-cache")]
+    let cache = Arc::new(
+        ConcurrentDiskCache::new(&dir, size, false, pool).expect("Failed to setup cache!"),
+    );
+    #[cfg(not(feature = "concurrent-cache"))]
+    let cache = Arc::new(DiskCache::new(&dir, size, pool, preprocessor_cache_mode_config));
+    Ok(cache)
 }
 
 #[cfg(test)]
